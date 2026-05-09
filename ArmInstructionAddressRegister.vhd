2f4986a7b22535befbbf723b329a31f47fa0c419
olti <olti.demiri0@gmail.com>
2026-05-09 14:26:29 +0200

    Aufgabe 2 done

--- ArmInstructionAddressRegister.vhd
+++ ArmInstructionAddressRegister.vhd
@@ -45,17 +45,44 @@ architecture behave of ArmInstructionAddressRegister is
         ARB_DATA_OUT : out std_logic_vector(ARB_DATA_WIDTH-1 downto 0)
         );
     end component ArmRamBuffer;
+    signal iar_reg: std_logic_vector(31 downto 2);
+    signal iar_next: std_logic_vector(31 downto 2);
+    signal history_data_out: std_logic_vector(31 downto 2);
+
+
 begin
+    iar_next <= std_logic_vector(unsigned(iar_reg) + 1);
+
+    IAR_ADDR_OUT <= iar_reg;
+    IAR_NEXT_ADDR_OUT <= history_data_out
+                     when IAR_REVOKE = '1'
+                     else iar_next;
+
     IAR_HISTORY_BUFFER: ArmRamBuffer
     generic map(
             ARB_ADDR_WIDTH => INSTRUCTION_ID_WIDTH,
             ARB_DATA_WIDTH => 30
         )
     port map(
-        ARB_CLK      =>
-        ARB_WRITE_EN =>
-        ARB_ADDR     =>
-        ARB_DATA_IN  =>
-        ARB_DATA_OUT =>
+        ARB_CLK      => IAR_CLK,
+        ARB_WRITE_EN => IAR_UPDATE_HB,
+        ARB_ADDR     => IAR_HISTORY_ID,
+        ARB_DATA_IN  => iar_reg,
+        ARB_DATA_OUT => history_data_out
     );
+
+    process (IAR_CLK)
+    begin
+        if rising_edge(IAR_CLK) then
+            if IAR_RST = '1' then
+                iar_reg <= VCR_RESET(31 downto 2);
+            elsif IAR_REVOKE = '1' then
+                iar_reg <= history_data_out;
+            elsif IAR_LOAD = '1' then
+                iar_reg <= IAR_ADDR_IN;
+            elsif IAR_INC = '1' then
+                iar_reg <= iar_next;
+            end if;
+        end if;
+    end process;
 end architecture behave;
